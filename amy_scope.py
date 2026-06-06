678730bb632e1facc85556551099fa26084e1557
Matthew Berland <mberland@wisc.edu>
2026-06-06 12:54:22 -0500

    fixed scope

--- amy_scope.py
+++ amy_scope.py
@@ -15,50 +15,64 @@ skips = 4
 ticks = 0
 last_encoder = -1
 
-HISTORY_LENGTH = 30
+HISTORY_LENGTH = 50
 X_MAX = 83
 X_BUF = 4
 
+history_scale = 100.0
+
+calibration_max = 1.0
+calibration_min = -1.0
+
+def randmax():
+    if random.random() < 0.5:
+        return 10
+    return -10
+
+history = [(randmax(),randmax()) for _ in range(HISTORY_LENGTH)]
+X_MULT = 125.0 / HISTORY_LENGTH
 
 
-history = [(random.randint(-10,10),random.randint(-10,10)) for _ in range(HISTORY_LENGTH)]
-X_MULT = 120 / HISTORY_LENGTH
-
-
-def draw_graph(data, y_mid=100, y_size=45):
+def draw_graphs(data):
     global X_MAX, X_BUF, X_MULT
-    y_min = y_mid + (y_size // 2)
-    hist_min = min(data)
-    hist_max = max(data)
-    hist_range = 1.0 * max(1, hist_max - hist_min)
-    val = (data[0] - hist_min) / hist_range
-    amyboard.display.text(f"{hist_max:+.1f}",X_BUF,y_mid-y_size//2,255)
-    amyboard.display.text(f"{hist_min:+.1f}",X_BUF,y_mid+(y_size//2)-5,255)
-    for i in range(len(data)):
+    cv1_data = [d[0] for d in data]
+    cv2_data = [d[1] for d in data]
+    all_data = cv1_data + cv2_data
+    hist_min = min(all_data)
+    hist_max = max(all_data)
+    hist_range = max(1, hist_max - hist_min)
+    cv1_scaled = [(d - hist_min) / (hist_range) for d in cv1_data]
+    cv2_scaled = [(d - hist_min) / (hist_range) for d in cv2_data]
+    y_size = 45.0
+    cv1y_min = 15    
+    cv2y_min = int(cv1y_min + y_size)
+    y_bottom = 128-20
+    amyboard.display.text(f"SCOPE",2,5,255)
+    amyboard.display.line(0,cv1y_min - 1,128,cv1y_min - 1,255)
+    amyboard.display.line(0,cv2y_min,128,cv2y_min,255)
+    amyboard.display.line(0,y_bottom,128,y_bottom,255)
+    amyboard.display.text(f"{hist_min/history_scale:+.1f} >> {hist_max/history_scale:+.1f}",2,y_bottom+5,255)
+
+    for i in range(len(data) - 1):
         x1 = X_BUF + int(i*X_MULT)
         x2 = x1 + 2
-        if (hist_max - hist_min) < 1:
-            y1 = y_mid
-            y2 = y_mid
+        
+        if hist_range < 2:
+            amyboard.display.text("NO DATA", 2, 70, 255)
         else:
-            last_val = val
-            val = (data[i] - hist_min) / hist_range
-            y1 = y_min - int(y_size * last_val)
-            y2 = y_min - int(y_size * val)
-        amyboard.display.line(x1,y1,x2,y2,255)
+            cv1y1 = cv1y_min + int(y_size * cv1_scaled[i])
+            cv1y2 = cv1y_min + int(y_size * cv1_scaled[i+1])
+            cv2y1 = cv2y_min + int(y_size * cv2_scaled[i])
+            cv2y2 = cv2y_min + int(y_size * cv2_scaled[i+1])
+            amyboard.display.line(x1,cv1y1,x2,cv1y2,255)
+            amyboard.display.line(x1,cv2y1,x2,cv2y2,255)
     amyboard.display.refresh()
 
 
 def update_display():
     global history
     amyboard.display.fill(0)
-    amyboard.display.text("CPL SCOPE", 2, 5, 255)
-    cv1s = map(lambda x: x[0], history)
-    cv2s = map(lambda x: x[1], history)
-    draw_graph(list(cv1s),43,48)
-    amyboard.display.line(0,15,128,15,255)
-    amyboard.display.line(0,70,128,70,255)
-    draw_graph(list(cv2s),98,48)
+    draw_graphs(history)
     amyboard.display_refresh()
 
 cv1 = 0.0
@@ -72,22 +86,10 @@ last_time = ctime
 
 def loop():
     global cv1, cv2, history, last_time, ctime
-    last_time = ctime
-    ctime = time.time()
-    if (ctime - last_time) < 0.05:
-        return
+
     cv1 = amyboard.cv_in(channel=0)
-    cv2 = amyboard.cv_in(channel=1)
-    if cv1 > 10 or cv1 < -10:
-        cv1 = 0.0
-    if cv2 > 10 or cv2 < -10:
-        cv2 = 0.0
-	
-    history.append((int(10 * cv1),int(10 * cv2)))
+    time.sleep(0.1)
+    cv2 = amyboard.cv_in(channel=1)	
+    history.append((int(history_scale * cv2),int(history_scale * cv1)))
     history.remove(history[0])
     update_display()
-    
-#    UPDATE_INTERVAL = 0.2
-#    last_time = ctime
-#    ctime = time.time()
-#    if (ctime - last_time) > UPDATE_INTERVAL:
